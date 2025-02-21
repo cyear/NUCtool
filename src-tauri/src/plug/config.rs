b4245e1a5dbb23ed8f1800feb3655d124de13ac2
冫氵丶面包 <chumuzero@gmail.com>
2025-02-21 19:35:30 +0800

    更新 config.rs

--- src-tauri/src/plug/config.rs
+++ src-tauri/src/plug/config.rs
@@ -1,9 +1,10 @@
-use crate::plug::struct_set::FanData;
 use std::{
-    fs::{self, Read, File},
+    io::Read,
+    fs::{self, File},
     path::PathBuf
 };
 
+use crate::plug::struct_set::FanData;
 pub fn get_config_dir() -> PathBuf {
     dirs::config_dir()
         .unwrap()
@@ -43,32 +44,22 @@ pub fn find_hwmon_with_name() -> PathBuf {
 
 #[tauri::command]
 pub async fn save_fan_config(fan_data: FanData) -> Result<(), String> {
-    // 获取配置文件路径
     let config_path = get_config_file_path()?;
-
-    // 将 fan_data 序列化为 JSON
     let json_data = serde_json::to_string_pretty(&fan_data).map_err(|e| e.to_string())?;
-
-    // 写入配置文件
     fs::write(config_path, json_data).map_err(|e| e.to_string())?;
-
     println!("风扇配置已保存");
     Ok(())
 }
 
 #[tauri::command]
 pub async fn load_fan_config() -> FanData {
-    // 获取配置文件路径
     let config_path = get_config_file_path().unwrap();
-    // 检查配置文件是否存在
     if !config_path.exists() {
         println!("配置文件不存在");
     }
-    // 读取配置文件
     let json_data = fs::read_to_string(config_path)
         .map_err(|e| e.to_string())
         .unwrap();
-    // 反序列化为 FanData
     let fan_data: FanData = serde_json::from_str(&json_data)
         .map_err(|e| e.to_string())
         .unwrap();
